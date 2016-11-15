8c0e153bcce4cf3f513d9fdeda381b80763d2c52
Caique Oliveira <j.caique_fla@hotmail.com>
2016-11-14 23:58:22 -0200

    finish feature login app

---
+++ app/src/main/java/doar/doe/app/Application.java
@@ -0,0 +1,13 @@
+package doar.doe.app;
+
+/**
+ * Created by Kanda on 08/11/2016.
+ */
+
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
---
+++ app/src/main/java/doar/doe/app/views/LoginActivity.java
@@ -0,0 +1,73 @@
+package doar.doe.app.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import doar.doe.app.R;
+import doar.doe.app.presenter.LoginPresenter;
+
+
+public class LoginActivity extends BaseActivity implements LoginPresenter.LoginStatus {
+    private CallbackManager callbackManager;
+    private LoginPresenter presenter;
+    @BindView(R.id.loginFacebook)
+    LoginButton loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        presenter = new LoginPresenter(this);
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(callbackManager, presenter.facebookCallback());
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        presenter.onStop();
+    }
+
+    @Override
+    public void loginWithSucess() {
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == LoginPresenter.RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                GoogleSignInAccount account = result.getSignInAccount();
+                presenter.authWithGoogle(account);
+            } else {
+                showDialogMessage(getString(R.string.err_message_login));
+            }
+        } else {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @OnClick({R.id.googleLoginSignInButton})
+    public void onClick(View v) {
+        presenter.sigInWithGoogle();
+    }
+}
